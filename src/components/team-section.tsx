67f44ca88904dae893dc726c5d54b8191f8b39fa
wwwacedo <eng.marianammacedo@gmail.com>
2025-06-05 20:05:04 -0300

    membros

--- src/components/team-section.tsx
+++ src/components/team-section.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardContent } from './ui/card'
 import { Users } from 'lucide-react'
+import Image from 'next/image'
 
 export default function TeamSection() {
 	return (
@@ -16,13 +17,13 @@ export default function TeamSection() {
               </p>
             </div>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-5 gap-6">
               <Card>
                 <CardContent className="pt-6 text-center">
                   <div className="mx-auto mb-4 h-20 w-20 overflow-hidden rounded-full bg-blue-100">
-                    <Users className="h-12 w-12 mx-auto mt-4 text-primary" />
+									<Image src="https://avatars.githubusercontent.com/u/17372417?v=4" alt="Gabriel" width={80} height={80} className="w-full h-full object-cover" />
                   </div>
-                  <h3 className="text-xl font-bold">Gabriel de Deus Bianchi</h3>
+                  <h3 className="text-lg font-bold">Gabriel de Deus Bianchi</h3>
                   <p className="text-muted-foreground">RM 97325</p>
                 </CardContent>
               </Card>
@@ -30,9 +31,9 @@ export default function TeamSection() {
               <Card>
                 <CardContent className="pt-6 text-center">
                   <div className="mx-auto mb-4 h-20 w-20 overflow-hidden rounded-full bg-blue-100">
-                    <Users className="h-12 w-12 mx-auto mt-4 text-primary" />
+									<Image src="https://avatars.githubusercontent.com/u/77203283?v=4" alt="Leonardo" width={80} height={80} className="w-full h-full object-cover" />
                   </div>
-                  <h3 className="text-xl font-bold">Leonardo Mazolini Fernandes</h3>
+                  <h3 className="text-lg font-bold">Leonardo Mazolini Fernandes</h3>
                   <p className="text-muted-foreground">RM 96897</p>
                 </CardContent>
               </Card>
@@ -40,29 +41,29 @@ export default function TeamSection() {
               <Card>
                 <CardContent className="pt-6 text-center">
                   <div className="mx-auto mb-4 h-20 w-20 overflow-hidden rounded-full bg-blue-100">
-                    <Users className="h-12 w-12 mx-auto mt-4 text-primary" />
+										<Image src="https://avatars.githubusercontent.com/u/79052641?v=4" alt="Mariana" width={80} height={80} className="w-full h-full object-cover" />
                   </div>
-                  <h3 className="text-xl font-bold">Mariana do Monte Macêdo</h3>
+                  <h3 className="text-lg font-bold">Mariana do Monte Macêdo</h3>
                   <p className="text-muted-foreground">RM 93240</p>
                 </CardContent>
               </Card>
 
-              <Card className="sm:col-span-2 lg:col-span-1">
+              <Card>
                 <CardContent className="pt-6 text-center">
                   <div className="mx-auto mb-4 h-20 w-20 overflow-hidden rounded-full bg-blue-100">
-                    <Users className="h-12 w-12 mx-auto mt-4 text-primary" />
+									<Image src="https://avatars.githubusercontent.com/u/113611162?v=4" alt="Marilia" width={80} height={80} className="w-full h-full object-cover" />
                   </div>
-                  <h3 className="text-xl font-bold">Marilia Fiorella Méjico Mendoza Bueno</h3>
+                  <h3 className="text-lg font-bold">Marilia Fiorella Méjico Mendoza Bueno</h3>
                   <p className="text-muted-foreground">RM 97300</p>
                 </CardContent>
               </Card>
 
-              <Card className="sm:col-span-2 lg:col-span-1">
+              <Card>
                 <CardContent className="pt-6 text-center">
                   <div className="mx-auto mb-4 h-20 w-20 overflow-hidden rounded-full bg-blue-100">
-                    <Users className="h-12 w-12 mx-auto mt-4 text-primary" />
+									<Image src="https://avatars.githubusercontent.com/u/100859801?v=4" alt="Victor" width={80} height={80} className="w-full h-full object-cover" />
                   </div>
-                  <h3 className="text-xl font-bold">Víctor Bonetti Pegoraro</h3>
+                  <h3 className="text-lg font-bold">Víctor Bonetti Pegoraro</h3>
                   <p className="text-muted-foreground">RM 97533</p>
                 </CardContent>
               </Card>
